185f9ff93d10f8348949eeef12761f454c31c0c2
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2020-11-27 10:21:59 +0100

    Rozwiązanie przykładu z decorator cache

--- ex4_extra.py
+++ ex4_extra.py
@@ -1,18 +1,26 @@
 #
 # Decorator cache
 #
-import time
 import functools
+import time
 
-def cache_decorator(func):
+def cached_decorator(func):
     cache = {}
-    pass
+    @functools.wraps(func)
+    def internal(*args, **kwargs):
+        key = args, tuple(kwargs)
+        if key not in cache:
+            print("CACHE MISS!!")
+            cache[key] = func(*args, **kwargs)
+        else:
+            print("CACHE HIT!!!")
+        return cache[key]
+    return internal
 
-@cache_decorator
+@cached_decorator
 def big_fat_function(a, b):
     time.sleep(2)
     return a + b
-
 # miss cache
 print(big_fat_function(2, 3))
 print(big_fat_function(2, 2))
